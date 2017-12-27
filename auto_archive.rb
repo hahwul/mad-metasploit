7628a1677c5dfcd5bbc07218545f2b6ad2bc45bc
hahwul <hahwul@gmail.com>
2017-12-28 01:55:24 +0900

    update

---
+++ auto_archive.rb
@@ -0,0 +1,28 @@
+require 'csv'
+require 'fileutils'
+
+puts "[-] Download index data.."
+system "curl --silent https://raw.githubusercontent.com/offensive-security/exploit-database/master/files_exploits.csv > files_exploits.csv"
+puts "[+] Download Complate!"
+puts "[-] Loading index data.."
+tpath = "./archive/"
+target = CSV.read('files_exploits.csv')
+puts "[+] Loaded"
+puts "[-] Check and Download Exploit codes.."
+i=0
+target.each do |row|
+ if row[2]["(Metasploit)"]
+   #puts row[2]
+   if File.file?(tpath+row[1])
+      puts ""
+   else
+      FileUtils.mkpath(File.dirname(tpath+row[1])+"/")
+      rbfile =  "https://raw.githubusercontent.com/offensive-security/exploit-database/master/"+row[1]
+      puts rbfile+" ==> "+tpath+row[1]
+      #system "curl --silent "+rbfile+" > "+tpath+row[1]
+   end
+ end
+ i+=1
+end
+
+puts "[+] Finish."
