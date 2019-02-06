a980839e54d0c6e3ccec29b1f24c5aee5bd16ba2
hahwul <hahwul@gmail.com>
2019-02-07 01:52:05 +0900

    fixed bug & add post modules

--- config/config.rb
+++ config/config.rb
@@ -1,3 +1,4 @@
-@@metasploit_path = '/opt/metasploit-framework/embedded/framework/'
+$metasploit_path = '/opt/metasploit-framework/embedded/framework/'
+
 #                    /usr/share/metasploit-framework
 #                    insert your metasploit path!
--- mad-metasploit.rb
+++ mad-metasploit.rb
@@ -4,18 +4,22 @@ require File.dirname(__FILE__) + '/config/config.rb' # Include Config File
 def custom_modules
   puts '[+] Sync Custom Modules'
   puts '[+] Auxiliary..'
-  system "rm -rf #{@@metasploit_path+'/modules/auxiliary/mad_metasploit'}"
-  system "cp mad-metasploit-modules/auxiliary #{@@metasploit_path+'/modules/auxiliary/mad_metasploit -r'}"
+  system "rm -rf #{$metasploit_path+'/modules/auxiliary/mad_metasploit'}"
+  system "cp mad-metasploit-modules/auxiliary #{$metasploit_path+'/modules/auxiliary/mad_metasploit -r'}"
 
   puts '[+] Exploits..'
-  system "rm -rf #{@@metasploit_path+'/modules/exploits/mad_metasploit'}"
-  system "cp mad-metasploit-modules/exploits #{@@metasploit_path+'/modules/exploits/mad_metasploit -r'}"
+  system "rm -rf #{$metasploit_path+'/modules/exploits/mad_metasploit'}"
+  system "cp mad-metasploit-modules/exploits #{$metasploit_path+'/modules/exploits/mad_metasploit -r'}"
+  
+  puts '[+] Posts..'
+  system "rm -rf #{$metasploit_path+'/modules/post/mad_metasploit'}"
+  system "cp mad-metasploit-modules/post #{$metasploit_path+'/modules/post/mad_metasploit -r'}"
 end
 
 def plugins
   puts '[+] Sync Custom Plugins'
-  system "rm -rf #{@@metasploit_path+'/plugins/mad_metasploit'}"
-  system "cp mad-metasploit-plugins #{@@metasploit_path+'/plugins/mad-metasploit -r'}"
+  system "rm -rf #{$metasploit_path+'/plugins/mad_metasploit'}"
+  system "cp mad-metasploit-plugins #{$metasploit_path+'/plugins/mad-metasploit -r'}"
 end
 
 def resource_scripts
@@ -23,7 +27,7 @@ def resource_scripts
 end
 
 puts '[+] Sync Mad-Metasploit Modules/Plugins/Resource-Script to Metasploit-framework'
-puts '[+] Metasploit-framewrk directory: '+@@metasploit_path
+puts '[+] Metasploit-framewrk directory: '+$metasploit_path
 puts '    (set ./conf/config.rb)'
 custom_modules
 plugins
